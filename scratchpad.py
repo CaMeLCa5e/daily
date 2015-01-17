c65a86251a78f8424418fc91f63e06064ce9d108
Jack Martin <JM273606@gmail.com>
2015-01-16 20:19:10 -0500

    updates

---
+++ scratchpad.py
@@ -0,0 +1,20 @@
+# scratchpad.py
+
+
+# #make a list 
+
+# print range(1,1001)
+for n in range(1,1001):
+	x = []
+	#take out all numbers divisable by 3 or 5.
+	if n % 3. == 0 or n % 5. == 0:
+		x += n
+
+
+	# elif n/3.:
+	# 	print n
+	# print n/5.
+
+
+
+
