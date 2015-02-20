1c706592add304ffb6234195dd7bb76c4217a849
Jack Martin <JM273606@gmail.com>
2015-02-19 22:51:26 -0500

    updates

---
+++ Euler1.py
@@ -0,0 +1,15 @@
+#! usr/bin/python
+y = []
+
+for x in range(1, 1000):
+	if x % 3 == 0 or x % 5 ==0:
+		y.append(x)
+print[x in range(1, 1000)]
+print x 
+print y
+print sum(y)
+	# elif x % 5 == 0:
+
+	# break
+		# print y
+
