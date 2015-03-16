a4855beb3cb706bbb0f9095e4760f76e2c368a7b
Jack Martin <JM273606@gmail.com>
2015-03-15 23:31:31 -0400

    updates

---
+++ globalsettingsdjango.py
@@ -0,0 +1,17 @@
+gettext_noop = lambda s: s
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+DEBUG_PROPAGATE_EXCEPTIONS = False
+
+USE_ETAGS = False
+
+ADMINS = []
+
+INTERNAL_IPS = []
+
+ALLOWED_HOSTS = []
+
+TIME_ZONE = 
+
