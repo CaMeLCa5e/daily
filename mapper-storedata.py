204ff339ca4c401b2c79d52076d2e76c9ffa7a74
Jack Martin <JM273606@gmail.com>
2015-03-26 22:08:06 -0400

    updates

---
+++ mapper-storedata.py
@@ -0,0 +1,13 @@
+#! usr/bin/env python
+
+def mapper():
+	for line in sys.stdin:
+
+		data = line.strip().split("\t")
+
+		if len(data) == 6:
+		
+			date, time, store, cost, payment = data
+
+			print "{0}\t{1}".format(store, cost)
+
