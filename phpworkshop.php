a17e839f91b5a640a9d1245a9818f400ea3609a2
Jack Martin <JM273606@gmail.com>
2015-03-28 22:50:09 -0400

    updates

---
+++ phpworkshop.php
@@ -0,0 +1,37 @@
+
+<?php
+echo "hello world/n";
+echo "hello world/n";
+echo "hello world/n";
+echo "hello world/n";
+echo "hello world/n";
+
+
+//create a variable
+$x = 5;
+$y = 10
+$z = $x + $y;
+
+echo "the sum of " . $x . "and" . $y . "is" . $z . "!";
+for ($i=0; $i<10; $i++) {
+	echo "the current value is " . $i;
+}
+
+while($i<10){
+	echo "current var" . $i .;
+		
+}
+
+// def a func
+function doSomething() {
+	echo "you called the function";
+	echo $msg;
+}
+
+doSomething();
+
+function doSometing3() {
+	return "<h2>Functions with return</h2> " 
+}
+
+?>
