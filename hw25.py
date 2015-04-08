65a2fb4e56e1bc2219a35ca6e4712376343d40b1
Jack Martin <JM273606@gmail.com>
2015-04-08 10:36:16 -0400

    updates

--- hw25.py
+++ hw25.py
@@ -29,16 +29,48 @@ $ python 2-15.py
 please enter a 4-digit year: 1945
 count 286, sum 32.55, avg 0.113811188811
 """
-int1 = raw_input('Please enter a 4-digit year: ')
 
-if not int1.isdigit():
+filename = "famaFrench.txt"
+count = 0
+sumOfFama = 0
+
+# Use raw_input() to take user input - a 4-digit year. 
+year = raw_input('Please enter a 4-digit year: ')
+
+# Reject the input with a polite message if it is not all digits. 
+if not year.isdigit():
 	exit('sorry, that was bad input')
 
-# elif not int1 == len(4):
-	# exit('argument must be a 4 character integer')
+for line in open('famaFrench.txt'):
+    line = line.rstrip()
+    line_words = line.split()
+	elements = line.split(' ')
+	# print elements[4]
+
+    line_year = line[0:4]
+    if line_year == year:
+	    count += 1
 
 
+if line_year == 0:
+	print 'no results were found for that year'
+
+average = sumOfFama / float(count)
 
 
+# format = count 301, sum 26.26, avg 0.0872425249169
+print 'count {0}, sum {1}, avg {2}'.format(count, sumOfFama, average)
+
+
+# print count
+	# if line_year == 0:
+		# print 'no results found for requested year'
+
+
+# for line in contact_list:
+#  	line = line.rstrip()
+
+ 	# print "{0} {1}".format(els[0], els[1])
+ 	# result_count += 1
 
 
